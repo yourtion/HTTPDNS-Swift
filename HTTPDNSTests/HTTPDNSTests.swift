bfd5e822d3665e1074ed08602eb978a6d78f92e2
YourtionGuo <yourtion@gmail.com>
2016-01-11 13:41:14 +0800

    simple test

---
+++ HTTPDNSTests/HTTPDNSTests.swift
@@ -0,0 +1,51 @@
+//
+//  HTTPDNSTests.swift
+//  HTTPDNSTests
+//
+//  Created by YourtionGuo on 1/10/16.
+//  Copyright © 2016 Yourtion. All rights reserved.
+//
+
+import XCTest
+@testable import HTTPDNS
+
+class HTTPDNS_iOSTests: XCTestCase {
+    
+    let HTTPDNS_CLASS = HTTPDNS()
+    let RES_Domain = "yourtion.com"
+    let RES_String = "192.243.118.110;192.243.118.111;192.243.118.112,600"
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testBasic() {
+        XCTAssertEqual(HTTPDNS_CLASS.SERVER_ADDRESS, "http://119.29.29.29/")
+        XCTAssertEqual(HTTPDNS_CLASS.cache.count, 0)
+    }
+    
+    func testGetRequestString () {
+        let REQ_String = HTTPDNS_CLASS.getRequestString(RES_Domain)
+        XCTAssertEqual(REQ_String, "http://119.29.29.29/d?dn=" + RES_Domain + "&ttl=1")
+    }
+    
+    func testParseResult() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let RES_Data = RES_String.dataUsingEncoding(NSUTF8StringEncoding)
+        let RES_Parsed = HTTPDNS_CLASS.parseResult(RES_Data!)
+        XCTAssertEqual(RES_Parsed.ip, "192.243.118.110")
+        XCTAssertEqual(RES_Parsed.ips.count, 3)
+        XCTAssertEqual(RES_Parsed.ips[0], "192.243.118.110")
+        XCTAssertEqual(RES_Parsed.ips[1], "192.243.118.111")
+        XCTAssertEqual(RES_Parsed.ips[2], "192.243.118.112")
+        XCTAssertEqual(RES_Parsed.ttl, 600)
+    }
+    
+}
