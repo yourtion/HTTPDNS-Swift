b007449659439c90aa0911cbb103a276d63620e9
YourtionGuo <yourtion@gmail.com>
2016-06-20 12:02:54 +0800

    add .travis.yml

--- HTTPDNS/HTTPDNS.h
+++ HTTPDNS/HTTPDNS.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double HTTPDNSVersionNumber;
 FOUNDATION_EXPORT const unsigned char HTTPDNSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <HTTPDNS/PublicHeader.h>
-
-
